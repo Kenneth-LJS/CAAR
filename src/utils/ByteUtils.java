9975fb3e62a091261ee3d5a64bcd9ad1071347f2
Ken Loh <Kenneth_LJS@Live.com>
2017-04-28 20:50:42 +0800

    Sound threshold experiment

--- src/utils/ByteUtils.java
+++ src/utils/ByteUtils.java
@@ -5,55 +5,55 @@ import java.nio.ByteOrder;
 
 public class ByteUtils {
 
-    public static byte[] shortToBytes(short s, boolean bigEndian) {
+    public static byte[] shortToBytes(short s, boolean isBigEndian) {
         ByteBuffer buffer = ByteBuffer.wrap(new byte[2]);
-        buffer.order(bigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
+        buffer.order(isBigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
         buffer.putShort(s);
         return buffer.array();
     }
     
-    public static short bytesToShort(byte[] b, int position, boolean bigEndian) {
+    public static short bytesToShort(byte[] b, int position, boolean isBigEndian) {
         ByteBuffer buffer = ByteBuffer.wrap(b, position, 2);
-        buffer.order(bigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
+        buffer.order(isBigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
         return buffer.getShort();
     }
     
-    public static byte[] intToBytes(int i, boolean bigEndian) {
+    public static byte[] intToBytes(int i, boolean isBigEndian) {
         ByteBuffer buffer = ByteBuffer.wrap(new byte[4]);
-        buffer.order(bigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
+        buffer.order(isBigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
         buffer.putInt(i);
         return buffer.array();
     }
     
-    public static int bytesToInt(byte[] b, int position, boolean bigEndian) {
+    public static int bytesToInt(byte[] b, int position, boolean isBigEndian) {
         ByteBuffer buffer = ByteBuffer.wrap(b, position, 4);
-        buffer.order(bigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
+        buffer.order(isBigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
         return buffer.getInt();
     }
     
-    public static byte[] floatToBytes(float f, boolean bigEndian) {
+    public static byte[] floatToBytes(float f, boolean isBigEndian) {
         ByteBuffer buffer = ByteBuffer.wrap(new byte[4]);
-        buffer.order(bigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
+        buffer.order(isBigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
         buffer.putFloat(f);
         return buffer.array();
     }
     
-    public static float bytesToFloat(byte[] b, int position, boolean bigEndian) {
+    public static float bytesToFloat(byte[] b, int position, boolean isBigEndian) {
         ByteBuffer buffer = ByteBuffer.wrap(b, position, 4);
-        buffer.order(bigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
+        buffer.order(isBigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
         return buffer.getFloat();
     }
     
-    public static byte[] doubleToBytes(double d, boolean bigEndian) {
+    public static byte[] doubleToBytes(double d, boolean isBigEndian) {
         ByteBuffer buffer = ByteBuffer.wrap(new byte[8]);
-        buffer.order(bigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
+        buffer.order(isBigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
         buffer.putDouble(d);
         return buffer.array();
     }
     
-    public static double bytesToDouble(byte[] b, int position, boolean bigEndian) {
+    public static double bytesToDouble(byte[] b, int position, boolean isBigEndian) {
         ByteBuffer buffer = ByteBuffer.wrap(b, position, 8);
-        buffer.order(bigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
+        buffer.order(isBigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN);
         return buffer.getDouble();
     }
     
