a187cf42c71912308c4bd242e23fb56c632c3528
Ken Loh <Kenneth_LJS@Live.com>
2017-04-28 13:55:54 +0800

    Vowel/consonant classification test

--- src/main/ContentAwareAudioResizer.java
+++ src/main/ContentAwareAudioResizer.java
@@ -31,7 +31,8 @@ public class ContentAwareAudioResizer {
 //                }
 //            }
 //        });
-        String[] files = new String[] { "ah", "eh", "ch" };
+        String[] files = new String[] { "ah", "eh", "ch", "ee", "oh", "u", "b", "ck", "t" };
+        //files = new String[] { "ee" };
         for (String file : files) {
             try {
                 AudioInputStream audioIn = AudioSystem.getAudioInputStream(new File(file + ".wav"));
@@ -65,7 +66,7 @@ public class ContentAwareAudioResizer {
                 for (int j = 0; j < samples.length; j++) {
                     samples[j] = Math.abs(samples[j]);
                 }
-                GraphUtils.dataToGraph(samples, "test3 - " + file + ".bmp");
+                GraphUtils.dataToGraph(samples, "file - " + file + ".bmp");
                 System.out.println(file + " : " + calcFormula(samples));
 //                System.out.println("Counted samples: " + count);
 //                System.out.println("Total samples: " + totalSamples);
@@ -102,17 +103,80 @@ public class ContentAwareAudioResizer {
     }
     
     public static float calcFormula(float[] in) {
-        // variance
-        double sum = 0;
-        for (int i = 0; i < in.length; i++) {
-            sum += in[i];
+        float[] arr = new float[in.length];
+        System.arraycopy(in, 0, arr, 0, in.length);
+        float max = Float.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
         }
-        float mean = (float)(sum / in.length);
-        sum = 0;
-        for (int i = 0; i < in.length; i++) {
-            sum += Math.pow(mean - in[i], 2);
+        max /= 20;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < max) {
+                arr[i] = 0;
+            }
         }
-        return (float)(sum / in.length);
+        int count = 0;
+        while (findTriangle(arr)) {
+            count++;
+        } 
+        return (float)count;
+//        try {
+//            GraphUtils.dataToGraph(arr, "formula2.bmp");
+//        } catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        // variance
+//        double sum = 0;
+//        for (int i = 0; i < in.length; i++) {
+//            sum += in[i] * i;
+//        }
+//        float mean = (float)(sum / in.length);
+//        sum = 0;
+//        for (int i = 0; i < in.length; i++) {
+//            sum += Math.pow(mean - in[i], 2);
+//        }
+//        return (float)(sum / in.length);
+    }
+    
+    
+    public static boolean findTriangle(float[] arr) {
+        int maxIndex = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        if (arr[maxIndex] == 0) {
+            return false;
+        }
+        int curWidth = 3; // tweak
+        int peakIndex = maxIndex;
+        float peakVal = arr[maxIndex];
+        float totalWidthCount = 0;
+        int pointCount = 0;
+        for (int i = Math.max(maxIndex - curWidth, 0); i <= peakIndex + curWidth; i++) {
+            if (i == peakIndex) {
+                continue;
+            }
+            totalWidthCount += peakVal * Math.abs(i - peakIndex) / (peakVal - arr[i]); 
+            pointCount++;
+        }
+        while (curWidth < totalWidthCount / pointCount) {
+            curWidth++;
+            if (peakIndex - curWidth >= 0) {
+                totalWidthCount += peakVal * curWidth / (peakVal - arr[peakIndex - curWidth]); 
+                pointCount++;
+            }
+            if (peakIndex + curWidth < arr.length) {
+                totalWidthCount += peakVal * curWidth / (peakVal - arr[peakIndex + curWidth]); 
+                pointCount++;
+            }
+        }
+        for (int i = Math.max(peakIndex - curWidth, 0); i <= Math.min(peakIndex + curWidth, arr.length - 1); i++) {
+            arr[i] = 0;
+        }
+        return true;
     }
     
     public static float[] filterRealComponent(float[] in) {
